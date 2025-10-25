4c9969836b7083394b5c7ee79f1d45fef6da1b93
tomakgmal-hue <tomakgmal@gmail.com>
2025-10-25 13:41:06 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-'use client';
-import AllPayHomepage from "@/components/AllPayHomepage";
+import { redirect } from 'next/navigation';
+
 export default function Page() {
-  return <AllPayHomepage />;
+  redirect('/en');
 }
