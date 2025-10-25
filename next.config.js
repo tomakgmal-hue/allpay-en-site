d825f3c20f20c4578148f82d267c40e857b44ead
tomakgmal-hue <tomakgmal@gmail.com>
2025-10-25 14:23:08 +0900

    Update next.config.js with TypeScript settings
    Added TypeScript configuration to ignore build errors.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
+  experimental: { typedRoutes: true },
+  typescript: {
+    ignoreBuildErrors: true
   }
 };
 module.exports = nextConfig;
